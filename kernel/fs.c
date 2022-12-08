069409d67a6ffcca9f4637254898f87ee07dc3c2
hhubibi <hxynobibi@gmail.com>
2022-12-08 19:09:44 +0800

    lab fs: finish large files

--- kernel/fs.c
+++ kernel/fs.c
@@ -397,7 +397,7 @@ bmap(struct inode *ip, uint bn)
   bn -= NDIRECT;
 
   if(bn < NINDIRECT){
-    // Load indirect block, allocating if necessary.
+    // Load singly-indirect block, allocating if necessary.
     if((addr = ip->addrs[NDIRECT]) == 0){
       addr = balloc(ip->dev);
       if(addr == 0)
@@ -414,6 +414,40 @@ bmap(struct inode *ip, uint bn)
       }
     }
     brelse(bp);
+    return addr;
+  }
+
+  bn -= NINDIRECT;
+  if(bn < NDINDIRECT){
+    // Load doubly-indirect block, allocating if necessary.
+    if((addr = ip->addrs[NDIRECT+1]) == 0){
+      addr = balloc(ip->dev);
+      if(addr == 0)
+        return 0;
+      ip->addrs[NDIRECT+1] = addr;
+    }
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    if((addr = a[bn/NINDIRECT]) == 0){
+      addr = balloc(ip->dev);
+      if(addr){
+        a[bn/NINDIRECT] = addr;
+        log_write(bp);
+      }
+    }
+    brelse(bp);
+    if(addr){
+      bp = bread(ip->dev, addr);
+      a = (uint*)bp->data;
+      if((addr = a[bn%NINDIRECT]) == 0){
+        addr = balloc(ip->dev);
+        if(addr){
+          a[bn%NINDIRECT] = addr;
+          log_write(bp);
+        }
+      }
+      brelse(bp);
+    }
     return addr;
   }
 
